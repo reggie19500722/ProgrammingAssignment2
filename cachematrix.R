233724e890c11cbd9a91489e959e18534a030ec1
reggie19500722 <reggiedavies@blueyonder.co.uk>
2014-12-19 14:50:48 +0000

    amended code v3

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,17 @@
 ## This package contains two functions:
 ##
-## makeCacheMatrix() creates new objects(s) and contains 2 get and 1 set function.
+## makeCacheMatrix() creates new objects(s) and contains 2 get and 2 set function.
 ##
 ## cacheSolve() accesses the object (not the makeCacheMatrix() function, but
 ## the object created when makeCaceMatrix was called). The inverse matrix is 
-## then calculated if, and only if, the inverse has not already been calculated.
+## then calculated if, and only if, the inverse has not already been calculated
+## and the determinant of the input matrix is nor equal to zero.
 
 ## makeCacheMatrix is passed a matrix which creates an "object" of type 'matrix'.
 ## This object stores two things, The original value of the matrix and what will
 ## be the cached value of its inverse, which is initially set to 'NULL'. Within
-## the makeCacheMatrix() function there are also three functions, two to read 
-## (or 'get') the values of the two things being stored, and one function to 
+## the makeCacheMatrix() function there are also four functions, two to read 
+## (or 'get') the values of the two things being stored, and two functions to 
 ## change ('set') them.
 
 makeCacheMatrix <- function(x = matrix()) {         # input will be a matrix
@@ -18,14 +19,24 @@ makeCacheMatrix <- function(x = matrix()) {         # input will be a matrix
     m <- NULL        # m will be our inverse matrix and it is reset to NULL 
     # every time makeCacheMatrix() is called.
     
-    # Note - these next three functions are defined but not run
-    # when makeCacheMtrix is called.
+    # Note - three of the following four functions are defined
+    # but not run when makeCacheMtrix is called.
     #
     # Instead, they will be used by cacheSolve() to get values
     # for x or for m (the inverse) and for setting the inverse.
+    # These are: get, setinv and getinv.
+    #
+    # The set function allows the user to assign a new value
+    # to the object, if that's necessary to save memory.
     
     get <- function() { x }     # this function returns the value of the original 
     # matrix.
+    
+    set <- function(y) {        # takes an input matrix
+        x <<- y             # saves the input matrix
+        m <<- NULL          # resets the inverse to NULL, whixh is what happerns
+        # when a new object is generated.
+    }
     
     setinv <- function(solve) { m <<- solve}
     # This is called by cacheSolve() during the first cacheSolve()
@@ -35,7 +46,8 @@ makeCacheMatrix <- function(x = matrix()) {         # input will be a matrix
     # cacheSolve() on subsequent accesses.
     
     list(get = get,                 # This is accessed each time makeCaceMatrix()
-         setinv = setinv,            # is called, that is, each time a new object.
+         set = set,                  # is called, that is, each time a new object
+         setinv = setinv,            # is made.
          getinv = getinv)            # This is the list of the internal functions
     # (methods) so a calling function knows how
     # to access these methods.
@@ -45,7 +57,7 @@ makeCacheMatrix <- function(x = matrix()) {         # input will be a matrix
 
 ## cacheSolve() accesses the object (not the makeCacheMatrix() function, but the object
 ## created when makeCacheMatrix() was called) by fetching the matrix used to create the
-## object, this matrix being stored when the object was created.
+## object, this matrix was stored when the object was created.
 ##
 ## If the inverse matrix has not been calculated (if it is still 'NULL') cacheSolve()
 ## calculates the inverse matrix, provided the determinant of the original matrix
@@ -55,7 +67,7 @@ makeCacheMatrix <- function(x = matrix()) {         # input will be a matrix
 ## the solve() function is used. A message is printed to the console advising the
 ## user that the matrix cannot be solved and the screen returns to the '>' input prompt.
 ## 
-## If the determinant does not equal zero then the inverse is calculated and cacheSolve()
+## If the determinant does not equal zero, the inverse is calculated and cacheSolve()
 ## stores the result in the object created by the call to cacheMakeMatrix(), and then returns
 ## the inverse matrix.
 ##
